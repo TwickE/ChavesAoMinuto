c3ac6c7424fcfce21befe34f4f1eb9f6835ed995
TwickE <118013263+TwickE@users.noreply.github.com>
2024-10-31 15:49:12 +0000

    Fixed the animation in the marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,45 +1,27 @@
-import { motion } from "framer-motion"
+import ReactMarquee from 'react-fast-marquee'
 import keyBrands from '../assets/keyBrands.js'
 import data from '../assets/keyBrands.json'
 
 function Marquee() {
-
-
     return (
-        <section className="container my-20 w-full mx-auto px-4 sm:px-20 overflow-x-hidden">
-            <div className="flex custom-gradient">
-                <motion.div
-                    initial={{ x: 0 }}
-                    animate={{ x: '-100%' }}
-                    transition={{ duration: 15, repeat: Infinity, ease: 'linear' }}
-                    className="flex flex-shrink-0"
-                >
-                    {data.map((brand, index) => (
+        <section className="my-20 w-full mx-auto px-4 sm:px-20 overflow-x-hidden">
+            <ReactMarquee
+                autoFill={true}
+                gradient={true}
+                gradientWidth={100}
+                speed={100}
+                pauseOnHover={true}
+            >
+                {data.map((brand, index) => (
+                    <a key={index} href={brand.link} target='_blank'>
                         <img
-                            key={index}
                             src={keyBrands[brand.name]}
-                            className='h-16 md:h-20 object-contain pr-20'
                             alt={brand.name}
+                            className='h-20 object-contain pr-20'
                         />
-                    ))}
-                </motion.div>
-
-                <motion.div
-                    initial={{ x: 0 }}
-                    animate={{ x: '-100%' }}
-                    transition={{ duration: 15, repeat: Infinity, ease: 'linear' }}
-                    className="flex flex-shrink-0"
-                >
-                    {data.map((brand, index) => (
-                        <img
-                            key={index}
-                            src={keyBrands[brand.name]}
-                            className='h-16 md:h-20 object-contain pr-20'
-                            alt={brand.name}
-                        />
-                    ))}
-                </motion.div>
-            </div>
+                    </a>
+                ))}
+            </ReactMarquee>
         </section>
     )
 }
