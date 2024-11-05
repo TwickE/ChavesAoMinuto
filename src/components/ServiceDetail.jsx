d3aff54b46b66406b3df10293064c8057643f1dc
TwickE <118013263+TwickE@users.noreply.github.com>
2024-11-05 10:13:31 +0000

    Update to the services images

--- src/components/ServiceDetail.jsx
+++ src/components/ServiceDetail.jsx
@@ -6,9 +6,9 @@ function ServiceDetail({ serviceData }) {
 
     return (
         <section className="px-4 w-full max-w-[1440px] mx-auto sm:px-20 md:px-40 mt-12 flex flex-col justify-center items-center gap-5">
-            <h2 className="text-primary-default text-2xl font-bold capitalize">{serviceData.name}</h2>
+            <h2 className="text-primary-default text-2xl text-center font-bold capitalize">{serviceData.name}</h2>
             <p className='text-gray-900 text-xl text-justify mb-5'>{serviceData.description}</p>
-            <img src={loadedImage} className='object-contain max-h-[40rem] border-8 border-primary-default rounded-xl mb-20' alt={serviceData.imageDescription} />
+            <img src={loadedImage} className='object-contain max-h-96 border-8 border-primary-default rounded-xl mb-20' alt={serviceData.imageDescription} />
         </section>
     )
 }
